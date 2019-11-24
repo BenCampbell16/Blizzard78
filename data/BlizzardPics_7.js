4ce7a44fcedfc5f9669b7b7bb53e31319fa09447
Ben Campbell <52384138+BenCampbell16@users.noreply.github.com>
2019-11-23 20:56:28 -0800

    Add files via upload
    Uploading Map

---
+++ data/BlizzardPics_7.js
@@ -0,0 +1,23 @@
+var json_BlizzardPics_7 = {
+"type": "FeatureCollection",
+"name": "BlizzardPics_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Providence, RI", "RelPath": "Providence78.jpg", "PhotoSrc": "Sami Tyrrell", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.414238207431779, 41.822671693902208 ] } },
+{ "type": "Feature", "properties": { "Name": "Woonsocket, RI", "RelPath": "WoonsocketBackyard78.jpg", "PhotoSrc": "Dahoov2 Wikipedia contribution", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.514551499954294, 42.002978707728467 ] } },
+{ "type": "Feature", "properties": { "Name": "Eastham, MA", "RelPath": "OuterMostHouseEastham78.jpg", "PhotoSrc": "Book by Henry Beston", "Desc": "The Outermost House, made famous by Beston's book of the same name was destroyed during the storm" }, "geometry": { "type": "Point", "coordinates": [ -69.943190687148316, 41.830640009073207 ] } },
+{ "type": "Feature", "properties": { "Name": "Boston, MA", "RelPath": "Boston78.jpg", "PhotoSrc": "City of Boston Archives", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.05507364645095, 42.356222015557464 ] } },
+{ "type": "Feature", "properties": { "Name": "Needham, MA", "RelPath": "Needham78.jpg", "PhotoSrc": "Jim McDevitt Photographer. U.S. Army Corps of Engineers", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.203681135560117, 42.266292071019095 ] } },
+{ "type": "Feature", "properties": { "Name": "Hartford, CT", "RelPath": "Hartford78.jpg", "PhotoSrc": "Arman G. Hatsian", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -72.673564017596021, 41.765759557924127 ] } },
+{ "type": "Feature", "properties": { "Name": "New Bedford, MA", "RelPath": "NewBedford78.png", "PhotoSrc": "Ron Rolo", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -70.92955285754033, 41.636175058906808 ] } },
+{ "type": "Feature", "properties": { "Name": "Rockport, MA", "RelPath": "Rockport_House78.png", "PhotoSrc": "Mass Government Archive", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -70.618215957762004, 42.658660850481454 ] } },
+{ "type": "Feature", "properties": { "Name": "Scituate, MA", "RelPath": "Scituate78.jpg", "PhotoSrc": "Elmer E Pooler", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -70.724790961725276, 42.196068824470643 ] } },
+{ "type": "Feature", "properties": { "Name": "Holliston, MA", "RelPath": "Holliston78.jpg", "PhotoSrc": "Elizabeth Moody", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.423849757559594, 42.200104076609556 ] } },
+{ "type": "Feature", "properties": { "Name": "North Providence, RI", "RelPath": "NorthProvidence78.jpg", "PhotoSrc": "Tony N. Ricci", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.468048516699099, 41.85371063435489 ] } },
+{ "type": "Feature", "properties": { "Name": "Stamford, CT", "RelPath": "Stamford78.jpg", "PhotoSrc": "The Stamford Historical Society", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -73.537082727849011, 41.051766765549615 ] } },
+{ "type": "Feature", "properties": { "Name": "Hope Valley, RI", "RelPath": "HopeValley78.jpg", "PhotoSrc": "Bob Graham", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.716364050551704, 41.507446345007999 ] } },
+{ "type": "Feature", "properties": { "Name": "Newport, RI", "RelPath": "Newport78.jpg", "PhotoSrc": "NewportRI.com", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.314098327221373, 41.478760609721967 ] } },
+{ "type": "Feature", "properties": { "Name": "Worcester, MA", "RelPath": "Worcester78.png", "PhotoSrc": "telegram.com", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -71.801795574029015, 42.263491882626901 ] } },
+{ "type": "Feature", "properties": { "Name": "New London, CT", "RelPath": "NewLondon.png", "PhotoSrc": "TheDay.com", "Desc": null }, "geometry": { "type": "Point", "coordinates": [ -72.100078748211288, 41.355252724945316 ] } }
+]
+}
